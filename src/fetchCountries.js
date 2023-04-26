cbe3644a8b897d38ea66ad722deedbba2df0f899
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-04-26 15:20:21 +0200

    all

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,26 +4,21 @@ import Notiflix from "notiflix";
 function fetchCountries(name) {
     // Побудова URL для запиту
     const url = `https://restcountries.com/v3.1/name/${name}`;
-    
     // Виконання fetch-запиту та повернення промісу з результатом
     return fetch(url)
         .then(resp => {
             // Перевірка статусу відповіді
             if (!resp.ok) {
-               Notiflix.Notify.failure(`HTTP-помилка! Статус: ${resp.status}`);
+               Notiflix.Notify.failure(`ERROR ${resp.status}`);
             }
-            // Парсинг відповіді у форматі JSON та повернення результату
+            // Парсинг відповіді у форматі JSON та повернення результату    
             return resp.json();
+            
         })
-        .then(data => {
-            // Повернення масиву країн з отриманих даних
-            return data;
-        })
-        .catch(error => {
-            // Обробка помилок
-            Notiflix.Notify.failure('Помилка отримання даних:', error);
-        });
-}
+       
+};
 
 // Експорт функції fetchCountries як іменованого експорту
+
 export { fetchCountries };
+
--- src/index.js
+++ src/index.js
@@ -1,28 +1,67 @@
-import './css/styles.css';
+import './css/styles.css';  
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
 
-import { fetchCountries } from './fetchCountries';
-
 const DEBOUNCE_DELAY = 300;
-
 const input = document.getElementById("search-box");
 const countryList = document.querySelector(".country-list");
 const countryInfo  = document.querySelector(".country-info");
 
-input.addEventListener("input", fetchCountries);
+input.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
 
-function showCountryInfo(country) {
-    countryInfo.innerHTML = `
-    <h2>${country.name}</h2>
-    <p>Capital: ${country.capital}</p>
-    <p>Population: ${country.population}</p>
-    <p>Region: ${country.region}</p>
-    <p>Languages: ${country.languages.join(", ")}</p>
-    `;
-
-};
-
-function showCountryList(countries) {
+function onInput(evt) {
+    const inputValue = evt.target.value.trim();
     countryList.innerHTML = "";
-    countries.forEach(showCountryInfo);
+    countryInfo.innerHTML = "";
+
+if (inputValue !=="") {
+    fetchCountries(inputValue)
+      .then(data => {
+       if (2 <= data.length  || data.length <=10){
+        const markup = data.map(country => 
+            `
+            <li class="list-group-item" clas = "flag" src =${country.flag.png} width = 80px> ${country.name.official}</li>
+            `
+        ).join(" ");
+        countryList.insertAdjacentHTML('beforeend',markup);
+       }
+      
+       if (data.length > 10){
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
 };
+console.log(data);  
+
+if (data.length === 1) {
+    const countryI = data.map(country => 
+        `<h2><img clas = "flag" src=${
+            country.flags.png
+          } width = 80px>  ${country.name.common} </h2>
+          <p>Capital: ${country.capital}</p>
+          <p>Population: ${country.population}</p>
+          <p>Languages: ${Object.values(country.languages)}</p>
+          <p>Region: ${country.region}</p>`
+            ).join('');
+            countryList.insertAdjacentHTML('beforeend',countryI);
+}
+}).catch(error => {
+    // Обробка помилок
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+});
+}
+};
+
+
+
+
+        // function showCountryInfo(country) {
+        //     countryInfo.innerHTML = `
+        //     <h2>${country.name}</h2>
+        //     <p>Capital: ${country.capital}</p>
+        //     <p>Population: ${country.population}</p>
+        //     <p>Region: ${country.region}</p>
+        //     <p>Languages: ${country.languages.join(", ")}</p>
+        //     `;
+        
+        // };
+        
